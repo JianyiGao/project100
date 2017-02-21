e84d25b002d38d34dada8731d558e23fda53b3f7
DESKTOP-9HS7O7H\Shandy Sulen <shandysulen>
2017-02-21 00:46:47 -0500

    Started working on tag functionality

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,27 @@
   angular.module('Project100', []);
 
 }());
+
+
+/* SHOW/HIDE TAGS*/
+$(document).ready(function(){
+	$("#accounts").click(function(){
+    $("account-tags").show();
+	});
+	
+	$("#research").click(function(){
+    $("p").show();
+	});
+	
+	$("#creative").click(function(){
+    $("p").show();
+	});
+	
+	$("#digital").click(function(){
+    $("p").show();
+	});
+	
+	$("#media").click(function(){
+    $("p").show();
+	});
+});
