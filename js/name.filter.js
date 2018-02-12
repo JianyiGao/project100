2a55e0b4744fc8b5a87700e37b7fd6133427e2c2
Trenton Ropp <trentonropp@gmail.com>
2018-02-12 18:41:18 -0500

    Project_new
    Newest files from server, isolated folder

---
+++ js/name.filter.js
@@ -0,0 +1,22 @@
+(function() {
+  angular.module('Project100')
+        .filter('name', NameFilter);
+
+  NameFilter.$inject = [];
+
+  function NameFilter(){
+    return function(seniorsArray){
+
+      var out = [];
+
+      angular.forEach(seniorsArray, function(senior){
+        var firstLetter = senior.name[0].toLowerCase();
+        if (firstLetter === 'w'){
+          out.push(senior)
+        }
+      });
+      return out;
+    }
+
+  }
+}());
---
+++ js/services/senior.service.js
@@ -0,0 +1,61 @@
+(function() {
+  angular.module('Project100')
+        .factory('SeniorService', SeniorService);
+
+  SeniorService.$inject = ['$http'];
+
+  function SeniorService($http){
+    init();
+    var seniors = [];
+
+    return {
+      get: getAllSeniors,
+      create: createOneSenior,
+      update: updateOneSenior,
+      delete: deleteOneSenior
+    };
+
+    function init(){ // this is going to make our first data request upon file load
+      $http.get('/seniors')
+      .then(function(response){
+        seniors = response.data.seniors;
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+    }
+
+    function getAllSeniors(){
+      return seniors;
+    }
+
+    function createOneSenior(senior){
+      $http.post('/seniors', senior)
+      .then(function(response){
+        seniors.push(senior);
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+    }
+
+    function updateOneSenior(index, updatedSenior){
+      $http.put('/seniors/', updatedSenior._id, updatedSenior)
+            .then(function(response){
+              seniors.splice(index, 1, updatedSenior);
+            })
+            .catch(function(){
+              console.log(err);
+            });
+    }
+    function deleteOneSenior(index, deletedSenior){
+      $http.delete('/seniors/', deletedSenior._id)
+          .then(function(){
+            seniors.splice(index, 1);
+          })
+          .catch(function(){
+            console.log(err);
+          });
+    }
+  }
+}());
